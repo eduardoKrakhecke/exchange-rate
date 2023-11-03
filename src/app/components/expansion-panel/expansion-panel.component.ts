4dabf5b2f85dfd3540656b13a05b82ea4afc92db
eduardoKrakhecke <eduardokg.mi6@gmail.com>
2023-11-03 16:44:59 -0300

    Ajuste expansion panel

--- src/app/components/expansion-panel/expansion-panel.component.ts
+++ src/app/components/expansion-panel/expansion-panel.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input  } from '@angular/core';
 
 @Component({
   selector: 'app-expansion-panel',
@@ -6,5 +6,10 @@ import { Component } from '@angular/core';
   styleUrls: ['./expansion-panel.component.scss']
 })
 export class ExpansionPanelComponent {
+  @Input() titulo: string;
+  isExpanded: boolean = false;
 
+  togglePanel() {
+    this.isExpanded = !this.isExpanded;
+  }
 }
